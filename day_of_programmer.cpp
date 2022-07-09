86120d4ce3ddbd584768cd35718b1b844107f89e
izanahmad8 <izan.khan596@gmail.com>
2022-07-09 11:35:56 +0530

    daily practice

---
+++ day_of_programmer.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int year;
+    cin >> year;
+    if (year > 1918)
+    {
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+        {
+            cout << "12.09." << year << endl;
+        }
+        else
+        {
+            cout << "13.09." << year << endl;
+        }
+    }
+    else if (year < 1918)
+    {
+
+        if (year % 4 == 0)
+        {
+            cout << "12.09." << year << endl;
+        }
+        else
+        {
+            cout << "13.09." << year << endl;
+        }
+    }
+    else
+    {
+        cout << "26.09." << year << endl;
+    }
+    return 0;
+}
