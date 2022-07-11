69ed53e5a596932472be3ad42cc4be0cf4feabe4
izanahmad8 <izan.khan596@gmail.com>
2022-07-11 20:30:25 +0530

    first java program

---
+++ java_if_else.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n=input.nextInt();   
+        if(n%2!=0)
+        {
+            System.out.println("Weird");
+        }     
+        else if(n%2==0 && n<=5 && n>=2)
+        {
+            System.out.println("Not Weird");
+        }
+        else if(n%2==0 && n<=20 && n>=6)
+        {
+            System.out.println("Weird");
+        }
+        else if(n%2==0 && n>20)
+        {
+            System.out.println("Not Weird");
+        }
+    }
+}
