327513218348c1a131093d44ec19c056283d39b4
izanahmad8 <izan.khan596@gmail.com>
2022-07-18 16:53:09 +0530

    daily practice

---
+++ find_the_median.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    sort(v.begin(), v.end());
+    int mid = n / 2;
+    cout << v[mid] << endl;
+    return 0;
+}
