4595bdf2aa25adbcf5056e836ed1e958cd3d75f4
izanahmad8 <izan.khan596@gmail.com>
2022-07-29 14:22:32 +0530

    daily practice

---
+++ closest_number.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    int diff[n];
+    adjacent_difference(arr, arr + n, diff);
+    int min = *min_element(diff + 1, diff + n);
+    for (int i = 1; i < n; i++)
+    {
+        if (diff[i] == min)
+        {
+            cout << arr[i - 1] << " " << arr[i] << " ";
+        }
+    }
+}
