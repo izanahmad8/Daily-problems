b2252e60a316dc8508f82503aa69ce1ff4a71ac5
izanahmad8 <izan.khan596@gmail.com>
2022-08-11 12:05:29 +0530

    daily practice

---
+++ insertion_sort_part2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 1; i < n; i++)
+    {
+        int j = i - 1;
+        while (arr[j] > arr[j + 1])
+        {
+            int temp = arr[j + 1];
+            arr[j + 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+}
