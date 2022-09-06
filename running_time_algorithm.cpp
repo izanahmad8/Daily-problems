acf9087415a8c62600a1c3a036e0263cbd4db3e2
izanahmad8 <izan.khan596@gmail.com>
2022-09-06 14:40:22 +0530

    daily practice

---
+++ running_time_algorithm.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,count=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        bool swapped=false;
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+                swapped=true;
+                count++;
+            }
+        }
+        if(swapped==false)
+        {
+            break;
+        }
+    }
+    cout<<count;
+}
+
