44cf07afeedc081657eea73dff7789d59adc99a2
izanahmad8 <izan.khan596@gmail.com>
2022-09-08 11:06:49 +0530

    daily practice

---
+++ Minimum_Absolute_Difference_in_an_Array.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    int min = 10e+7;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] - arr[i - 1] < min)
+        {
+            min = arr[i] - arr[i - 1];
+        }
+    }
+    cout << min;
+    return 0;
+}
