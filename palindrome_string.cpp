c1ac064849a597755a660508031a218d458ef0c6
izanahmad8 <izan.khan596@gmail.com>
2022-09-29 12:24:42 +0530

    dpp

---
+++ palindrome_string.cpp
@@ -0,0 +1,14 @@
+int isPalindrome(string S)
+{
+    int i = 0, j = S.size() - 1;
+    while (i < j)
+    {
+        if (S[i] != S[j])
+        {
+            return 0;
+        }
+        i++;
+        j--;
+    }
+    return 1;
+}
