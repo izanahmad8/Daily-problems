69f157d0139069562288fdf0b3f745e3a1ac3ef6
izanahmad8 <izan.khan596@gmail.com>
2023-07-24 11:36:03 +0530

    dpp

---
+++ printing_pattern_using_loops.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i < n * 2; i++)
+    {
+        for (int j = 1; j < 2 * n; j++)
+            printf("%d ", max(abs(n - i) + 1, abs(n - j) + 1));
+        printf("\n");
+    }
+    return 0;
+}
