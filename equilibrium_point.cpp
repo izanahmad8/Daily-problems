7d7acf9bfe3515f7e9f366d1bb854ff59d3fe52f
izanahmad8 <izan.khan596@gmail.com>
2023-08-04 14:04:21 +0530

    dpp

---
+++ equilibrium_point.cpp
@@ -0,0 +1,20 @@
+int equilibriumPoint(long long a[], int n)
+{
+
+    long sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += a[i];
+    }
+    long leftsum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (leftsum == sum - a[i])
+        {
+            return i + 1;
+        }
+        leftsum += a[i];
+        sum -= a[i];
+    }
+    return -1;
+}
