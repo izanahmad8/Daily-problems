69caf83c384bc0f714341f1d178f0a63c5714932
izanahmad8 <izan.khan596@gmail.com>
2023-08-06 11:31:46 +0530

    dpp

---
+++ first_repeating_element.cpp
@@ -0,0 +1,16 @@
+int firstRepeated(int arr[], int n)
+{
+    unordered_map<int, int> m;
+    for (int i = 0; i < n; i++)
+    {
+        m[arr[i]]++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (m[arr[i]] > 1)
+        {
+            return i + 1;
+        }
+    }
+    return -1;
+}
