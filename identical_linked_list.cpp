7ecb7cb2c3da01625391126ce1994b4f1eaf5532
izanahmad8 <izan.khan596@gmail.com>
2023-08-12 18:17:11 +0530

    dpp

---
+++ identical_linked_list.cpp
@@ -0,0 +1,20 @@
+bool areIdentical(struct Node *head1, struct Node *head2)
+{
+    while (head1 != NULL)
+    {
+        if (head1->data != head2->data)
+        {
+            return false;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    if (head2 == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
