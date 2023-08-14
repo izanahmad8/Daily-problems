96eb0dc1b7def7bb7836a566fbda2bae9505f220
izanahmad8 <izan.khan596@gmail.com>
2023-08-14 23:35:47 +0530

    dpp

---
+++ exceptionally_odd.cpp
@@ -0,0 +1,16 @@
+int getOddOccurrence(int arr[], int n)
+{
+    unordered_map<int, int> m;
+    for (int i = 0; i < n; i++)
+    {
+        m[arr[i]]++;
+    }
+    for (auto i : m)
+    {
+        if (i.second % 2 != 0)
+        {
+            return i.first;
+        }
+    }
+    return -1;
+}
