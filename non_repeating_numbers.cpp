88e80accaff6bc5d436d1a61d4d1f34b65d20ea4
izanahmad8 <izan.khan596@gmail.com>
2023-08-14 23:28:42 +0530

    dpp

---
+++ non_repeating_numbers.cpp
@@ -0,0 +1,17 @@
+vector<int> singleNumber(vector<int> nums)
+{
+    vector<int> ans;
+    map<int, int> m;
+    for (auto i : nums)
+    {
+        m[i]++;
+    }
+    for (auto i : m)
+    {
+        if (i.second == 1)
+        {
+            ans.push_back(i.first);
+        }
+    }
+    return ans;
+}
