68304653beb5e083ece752a730834fe7d9de29ae
izanahmad8 <izan.khan596@gmail.com>
2023-08-21 23:27:56 +0530

    dpp

---
+++ k_largest_elements.cpp
@@ -0,0 +1,10 @@
+vector<int> kLargest(int arr[], int n, int k)
+{
+    sort(arr, arr + n, greater<int>());
+    vector<int> ans;
+    for (int i = 0; i < k; i++)
+    {
+        ans.push_back(arr[i]);
+    }
+    return ans;
+}
