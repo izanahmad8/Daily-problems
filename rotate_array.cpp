14952ece8deabdcb0f83df480ce183ddccc49bd2
izanahmad8 <izan.khan596@gmail.com>
2023-09-05 10:47:12 +0530

    dpp

--- rotate_array.cpp
+++ rotate_array.cpp
@@ -5,3 +5,12 @@ void rotateArr(int arr[], int d, int n)
     reverse(arr + d, arr + n);
     reverse(arr, arr + n);
 }
+
+// leetcode
+void rotate(vector<int> &nums, int k)
+{
+    k = k % nums.size();
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+}
