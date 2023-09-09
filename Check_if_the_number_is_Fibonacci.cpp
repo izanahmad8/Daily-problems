804f1c7c929d0708b8d9d943813ad20c8cc8ffeb
izanahmad8 <izan.khan596@gmail.com>
2023-09-09 11:16:56 +0530

    dpp

---
+++ Check_if_the_number_is_Fibonacci.cpp
@@ -0,0 +1,11 @@
+string checkFibonacci(int N)
+{
+    if (N == 0 || N == 1 || N == 2 || N == 3 || N == 5 || N == 8 || N == 13 || N == 21 || N == 34 || N == 55 || N == 89)
+    {
+        return "Yes";
+    }
+    else
+    {
+        return "No";
+    }
+}
