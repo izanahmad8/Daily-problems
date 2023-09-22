423621aa3a3aaf0f1bca297a6b5ebb087d788c48
izanahmad8 <izan.khan596@gmail.com>
2023-09-22 18:16:50 +0530

    dpp

---
+++ find_the_duplicate_number.cpp
@@ -0,0 +1,16 @@
+int findDuplicate(vector<int> &nums)
+{
+    int n = nums.size();
+
+    for (int i = 0; i < n; ++i)
+    {
+        int ind = abs(nums[i]);
+
+        if (nums[ind] < 0)
+            return ind;
+
+        nums[ind] = -nums[ind];
+    }
+
+    return n;
+}
