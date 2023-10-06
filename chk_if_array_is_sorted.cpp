e648ce95d80335b3cf69fede789f74ace6650125
izanahmad8 <izan.khan596@gmail.com>
2023-10-06 18:25:24 +0530

    dpp

---
+++ chk_if_array_is_sorted.cpp
@@ -0,0 +1,11 @@
+bool arraySortedOrNot(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < arr[i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
