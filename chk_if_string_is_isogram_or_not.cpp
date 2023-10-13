b85e8d723bb5ba4aa0bd6053f3226165c7a35755
izanahmad8 <izan.khan596@gmail.com>
2023-10-13 13:30:25 +0530

    dpp

---
+++ chk_if_string_is_isogram_or_not.cpp
@@ -0,0 +1,16 @@
+bool isIsogram(string s)
+{
+    unordered_map<char, int> m;
+    for (int i = 0; i < s.size(); i++)
+    {
+        m[s[i]]++;
+    }
+    for (auto i : m)
+    {
+        if (i.second > 1)
+        {
+            return false;
+        }
+    }
+    return true;
+}
