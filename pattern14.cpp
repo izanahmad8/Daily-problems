6d3de4b55bd3f86b0b370a0dbb45a721768ba1f5
izanahmad8 <izan.khan596@gmail.com>
2023-11-28 11:37:28 +0530

    dpp

---
+++ pattern14.cpp
@@ -0,0 +1,11 @@
+void printTriangle(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (char j = 'A'; j < 'A' + i; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+}
