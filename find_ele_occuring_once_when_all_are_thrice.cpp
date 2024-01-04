e8c04aa7ed882fba817414137ef1dc900457dddf
izanahmad8 <izan.khan596@gmail.com>
2024-01-04 19:21:32 +0530

    dpp

---
+++ find_ele_occuring_once_when_all_are_thrice.cpp
@@ -0,0 +1,16 @@
+int singleElement(int arr[], int N)
+{
+    unordered_map<int, int> m;
+    for (int i = 0; i < N; i++)
+    {
+        m[arr[i]]++;
+    }
+    for (auto i : m)
+    {
+        if (i.second == 1)
+        {
+            return i.first;
+        }
+    }
+    return -1;
+}
