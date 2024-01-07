65b03925ad4a50a83e76cd12643d80c9bc946a32
izanahmad8 <izan.khan596@gmail.com>
2024-01-07 16:49:43 +0530

    dpp

---
+++ find_the_index_of_first_occurence_in_a_string.cpp
@@ -0,0 +1,18 @@
+int strStr(string haystack, string needle)
+{
+    if (haystack.size() < needle.size())
+    {
+        return -1;
+    }
+    for (int i = 0; i < haystack.size() - needle.size() + 1; i++)
+    {
+        if (haystack[i] == needle[0])
+        {
+            if (haystack.substr(i, needle.size()) == needle)
+            {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
