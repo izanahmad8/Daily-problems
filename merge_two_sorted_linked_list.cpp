56620fc0db06f5a2c1b78c05b88559019d474efb
izanahmad8 <izan.khan596@gmail.com>
2024-01-08 12:23:06 +0530

    dpp

---
+++ merge_two_sorted_linked_list.cpp
@@ -0,0 +1,34 @@
+Node *sortedMerge(Node *head1, Node *head2)
+{
+    Node *p1 = head1;
+    Node *p2 = head2;
+    Node *newlink = new Node(-1);
+    Node *p3 = newlink;
+    while (p1 != NULL && p2 != NULL)
+    {
+        if (p1->data < p2->data)
+        {
+            p3->next = p1;
+            p1 = p1->next;
+        }
+        else
+        {
+            p3->next = p2;
+            p2 = p2->next;
+        }
+        p3 = p3->next;
+    }
+    while (p1 != NULL)
+    {
+        p3->next = p1;
+        p1 = p1->next;
+        p3 = p3->next;
+    }
+    while (p2 != NULL)
+    {
+        p3->next = p2;
+        p2 = p2->next;
+        p3 = p3->next;
+    }
+    return newlink->next;
+}
