3943a3469bb5fc1ea1f0a8388a46e3f6cbb2352f
izanahmad8 <izan.khan596@gmail.com>
2024-01-16 11:25:03 +0530

    dpp

---
+++ sequence_of_sequence.cpp
@@ -0,0 +1,12 @@
+int numberSequence(int m, int n)
+{
+    if (m < n)
+    {
+        return 0;
+    }
+    if (n == 0)
+    {
+        return 1;
+    }
+    return numberSequence(m / 2, n - 1) + numberSequence(m - 1, n);
+}
