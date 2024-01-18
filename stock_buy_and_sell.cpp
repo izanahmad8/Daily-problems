325f828dd1b77e0f2bc2e4dbdd9d2b39dfd5e86d
izanahmad8 <izan.khan596@gmail.com>
2024-01-18 16:40:23 +0530

    dpp

---
+++ stock_buy_and_sell.cpp
@@ -0,0 +1,12 @@
+vector<vector<int>> stockBuySell(vector<int> A, int n)
+{
+    vector<vector<int>> ans;
+    for (int i = 1; i < n; i++)
+    {
+        if (A[i] > A[i - 1])
+        {
+            ans.push_back({i - 1, i});
+        }
+    }
+    return ans;
+}
