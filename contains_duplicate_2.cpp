7cb2003e9ce874d8995b1e34f1d0893ae9fd43ee
izanahmad8 <izan.khan596@gmail.com>
2024-02-10 22:40:15 +0530

    dpp

---
+++ contains_duplicate_2.cpp
@@ -0,0 +1,16 @@
+bool containsNearbyDuplicate(vector<int> &nums, int k)
+{
+    unordered_map<int, int> m;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (m.count(nums[i]))
+        {
+            if (abs(i - m[nums[i]] <= k))
+            {
+                return true;
+            }
+        }
+        m[nums[i]] = i;
+    }
+    return false;
+}
