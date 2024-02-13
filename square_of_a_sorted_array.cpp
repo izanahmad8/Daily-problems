1c026db4d21f182d0b27e621dd24aa25117faaea
izanahmad8 <izan.khan596@gmail.com>
2024-02-13 11:01:38 +0530

    dpp

---
+++ square_of_a_sorted_array.cpp
@@ -0,0 +1,21 @@
+vector<int> sortedSquares(vector<int> &nums)
+{
+    vector<int> ans;
+    int l = 0;
+    int r = nums.size() - 1;
+    while (l <= r)
+    {
+        if (abs(nums[l]) > abs(nums[r]))
+        {
+            ans.push_back(nums[l] * nums[l]);
+            l++;
+        }
+        else
+        {
+            ans.push_back(nums[r] * nums[r]);
+            r--;
+        }
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
