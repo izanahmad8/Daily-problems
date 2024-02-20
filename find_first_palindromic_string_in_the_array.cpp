153c3abc4545b55a172b9a222cacfec78671344a
Izan Ahmad <izan.khan596@gmail.com>
2024-02-20 21:38:41 +0530

    dpp

---
+++ find_first_palindromic_string_in_the_array.cpp
@@ -0,0 +1,25 @@
+string firstPalindrome(vector<string> &words)
+{
+    for (auto s : words)
+    {
+        if (isPalindrome(s))
+        {
+            return s;
+        }
+    }
+    return "";
+}
+bool isPalindrome(string s)
+{
+    int i = 0, j = s.size() - 1;
+    while (i < j)
+    {
+        if (s[i] != s[j])
+        {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
