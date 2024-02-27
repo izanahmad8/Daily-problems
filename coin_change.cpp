0091d3d0e3cb9e2b1e84c5eb90778b6626634bc4
Izan Ahmad <izan.khan596@gmail.com>
2024-02-27 09:51:23 +0530

    dpp

--- coin_change.cpp
+++ coin_change.cpp
@@ -26,3 +26,43 @@ long long int solve(int coins[], int N, int sum, vector<vector<long long int>> &
     }
     return dp[N][sum] = take + notake;
 }
+
+// leetcode
+
+int coinChange(vector<int> &coins, int amount)
+{
+    int n = coins.size();
+    vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
+    int ans = solve(coins, n, amount, dp);
+    if (ans == INT_MAX - 1)
+    {
+        return -1;
+    }
+    else
+    {
+        return ans;
+    }
+}
+int solve(vector<int> &coins, int n, int amount, vector<vector<int>> &dp)
+{
+    if (amount == 0)
+    {
+        return 0;
+    }
+    if (n == 0)
+    {
+        return INT_MAX - 1;
+    }
+    if (dp[n][amount] != -1)
+    {
+        return dp[n][amount];
+    }
+    if (coins[n - 1] <= amount)
+    {
+        return dp[n][amount] = min(1 + solve(coins, n, amount - coins[n - 1], dp), solve(coins, n - 1, amount, dp));
+    }
+    else
+    {
+        return dp[n][amount] = solve(coins, n - 1, amount, dp);
+    }
+}
