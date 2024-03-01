248eefa000d78ecca53b233b4dd9e07e4e0065e2
Izan Ahmad <izan.khan596@gmail.com>
2024-03-01 19:28:41 +0530

    dpp

--- peak_element.cpp
+++ peak_element.cpp
@@ -19,3 +19,23 @@ int peakElement(int arr[], int n)
     }
     return -1;
 }
+
+int peakElement(int arr[], int n)
+{
+    if (n == 1)
+        return 0;
+    int low = 0, high = n - 1;
+    while (low < high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] >= arr[mid + 1])
+        {
+            high = mid;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
