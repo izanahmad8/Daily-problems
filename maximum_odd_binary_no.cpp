f78f46253bd981342d2fa951b87339f69b0a0569
Izan Ahmad <izan.khan596@gmail.com>
2024-03-01 19:43:23 +0530

    dpp

---
+++ maximum_odd_binary_no.cpp
@@ -0,0 +1,13 @@
+string maximumOddBinaryNumber(string s)
+{
+    sort(s.begin(), s.end(), greater<int>());
+    int n = s.size();
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (s[i] == '1')
+        {
+            swap(s[i], s[n - 1]);
+        }
+    }
+    return s;
+}
