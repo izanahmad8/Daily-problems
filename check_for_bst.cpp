aeb57dfca395c5ab8744bf3526ad03099fcf7d08
Izan Ahmad <izan.khan596@gmail.com>
2024-03-15 13:49:16 +0530

    dpp

---
+++ check_for_bst.cpp
@@ -0,0 +1,23 @@
+bool isBST(Node *root)
+{
+    vector<int> v;
+    inorder(root, v);
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+        if (v[i] >= v[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+void inorder(Node *root, vector<int> &v)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left, v);
+    v.push_back(root->data);
+    inorder(root->right, v);
+}
