ce130ee4274b0ca71bb63b7871408a34190210ed
Izan Ahmad <izan.khan596@gmail.com>
2024-03-16 11:50:56 +0530

    dpp

--- delete_without_head_pointer.cpp
+++ delete_without_head_pointer.cpp
@@ -3,3 +3,9 @@ void deleteNode(Node *del)
     del->data = del->next->data;
     del->next = del->next->next;
 }
+void deleteNode(Node *del_node)
+{
+    Node *curr = del_node;
+    curr->data = curr->next->data;
+    curr->next = curr->next->next;
+}
