c80821e951c56a08c09ff24f9e47d5b4e78607ab
Izan Ahmad <izan.khan596@gmail.com>
2024-03-18 23:07:20 +0530

    dpp

---
+++ level_order_traversal.cpp
@@ -0,0 +1,21 @@
+vector<int> levelOrder(Node *root)
+{
+    vector<int> v;
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+        v.push_back(curr->data);
+        if (curr->left != NULL)
+        {
+            q.push(curr->left);
+        }
+        if (curr->right != NULL)
+        {
+            q.push(curr->right);
+        }
+    }
+    return v;
+}
