6288afbe0c7b0c811cf61e665227e4fdb787db9d
Izan Ahmad <izan.khan596@gmail.com>
2024-03-26 14:46:26 +0530

    dpp

---
+++ first_missing_positive.cpp
@@ -0,0 +1,17 @@
+int firstMissingPositive(vector<int> &nums)
+{
+    unordered_map<int, bool> mp;
+    int maxi = *max_element(nums.begin(), nums.end());
+    for (auto num : nums)
+    {
+        mp[num] = true;
+    }
+    for (int i = 1; i <= maxi; i++)
+    {
+        if (mp.find(i) == mp.end())
+        {
+            return i;
+        }
+    }
+    return maxi < 0 ? 1 : maxi + 1;
+}
