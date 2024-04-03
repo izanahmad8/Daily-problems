855216d853a667339d6efda3a0f4aafbd64ed64b
Izan Ahmad <izan.khan596@gmail.com>
2024-04-03 23:00:48 +0530

    dpp

---
+++ lowest_common_ancestor_in_a_BT.cpp
@@ -0,0 +1,16 @@
+Node *lca(Node *root, int n1, int n2)
+{
+    if (!root)
+        return NULL;
+    if (root->data == n1 || root->data == n2)
+    {
+        return root;
+    }
+    Node *left = lca(root->left, n1, n2);
+    Node *right = lca(root->right, n1, n2);
+    if (!left)
+        return right;
+    if (!right)
+        return left;
+    return root;
+}
