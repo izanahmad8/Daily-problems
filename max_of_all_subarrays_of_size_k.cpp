9aa27933918f529eac4c89ac0a152e8142b529cf
Izan Ahmad <izan.khan596@gmail.com>
2024-04-09 19:32:32 +0530

    dpp

---
+++ max_of_all_subarrays_of_size_k.cpp
@@ -0,0 +1,29 @@
+vector<int> max_of_subarrays(int *arr, int n, int k)
+{
+    vector<int> ans;
+    int i = 0;
+    int j = 0;
+    int maxi = -1;
+    while (j < n)
+    {
+        if (arr[j] > maxi)
+        {
+            maxi = arr[j];
+        }
+        if (j - i + 1 == k)
+        {
+            ans.push_back(maxi);
+            if (arr[i] == maxi)
+            {
+                maxi = -1;
+                j = i;
+            }
+            i++;
+        }
+        if (j - i + 1 != k)
+        {
+            j++;
+        }
+    }
+    return ans;
+}
