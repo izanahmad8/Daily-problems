9706488d3a46266a9ad16332bb25c6fe7a69b543
Izan Ahmad <izan.khan596@gmail.com>
2024-04-25 23:03:38 +0530

    dpp

---
+++ return_two_prime_numbers.cpp
@@ -0,0 +1,30 @@
+vector<int> primeDivision(int N)
+{
+    vector<bool> prime(N + 1, false);
+    vector<int> ans;
+    sieve(N, prime);
+    for (int i = 2; i <= N; i++)
+    {
+        if (prime[i] == false && prime[N - i] == false)
+        {
+            ans.push_back(i);
+            ans.push_back(N - i);
+            return ans;
+        }
+    }
+    return ans;
+}
+void sieve(int n, vector<bool> &prime)
+{
+    prime[0] = prime[1] = true;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (!prime[i])
+        {
+            for (int j = 2 * i; j <= n; j += i)
+            {
+                prime[j] = true;
+            }
+        }
+    }
+}
