30d0d90fd18fa1c48954850ce38022d76315bacc
Izan Ahmad <izan.khan596@gmail.com>
2024-05-02 17:39:49 +0530

    leetcode

---
+++ largest_positive_integer_that_exist_with_its_negative.cpp
@@ -0,0 +1,12 @@
+int findMaxK(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+    for (int i = nums.size() - 1; i >= 0; i--)
+    {
+        if (nums[i] > 0 && binary_search(nums.begin(), nums.end(), -nums[i]))
+        {
+            return nums[i];
+        }
+    }
+    return -1;
+}
