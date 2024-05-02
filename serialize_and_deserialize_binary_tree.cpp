943ae6b0075b445079b18c975750549ddfe3154d
Izan Ahmad <izan.khan596@gmail.com>
2024-05-02 17:58:12 +0530

    dpp

---
+++ serialize_and_deserialize_binary_tree.cpp
@@ -0,0 +1,36 @@
+Node *solve(vector<int> &A, int &ind)
+{
+    if (A[ind] == -1)
+    {
+        ind++;
+        return NULL;
+    }
+    Node *root = new Node(A[ind]);
+    ind++;
+    root->left = solve(A, ind);
+    root->right = solve(A, ind);
+    return root;
+}
+void pre(Node *root, vector<int> &ans)
+{
+    if (!root)
+    {
+        ans.push_back(-1);
+        return;
+    }
+    ans.push_back(root->data);
+    pre(root->left, ans);
+    pre(root->right, ans);
+    return;
+}
+vector<int> serialize(Node *root)
+{
+    vector<int> ans;
+    pre(root, ans);
+    return ans;
+}
+Node *deSerialize(vector<int> &A)
+{
+    int ind = 0;
+    return solve(A, ind);
+}
