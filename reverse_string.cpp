720af5678ca1ed209a49b99151fae72bdb955cde
Izan Ahmad <izan.khan596@gmail.com>
2024-06-02 23:25:34 +0530

    leetcode

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -6,3 +6,14 @@ public:
         reverse(s.begin(), s.end());
     }
 };
+
+void reverseString(vector<char> &s)
+{
+    int l = 0, r = s.size() - 1;
+    while (l < r)
+    {
+        swap(s[l], s[r]);
+        l++;
+        r--;
+    }
+}
