6056cae5cb1b5ba3d6e2334b1016181924ea3b52
Izan Ahmad <izan.khan596@gmail.com>
2024-06-03 22:53:38 +0530

    leetcode

---
+++ append_character_to_make_string_subsequence.cpp
@@ -0,0 +1,19 @@
+int appendCharacters(string s, string t)
+{
+    int i = 0, j = 0;
+    while (i < s.size())
+    {
+        if (s[i] == t[j])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    if (j == t.size())
+        return 0;
+    return t.size() - j;
+}
