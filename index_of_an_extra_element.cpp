9a3105e928573865b1be5f4e0feb0da92459293c
Izan Ahmad <izan.khan596@gmail.com>
2024-06-08 20:28:53 +0530

    dpp

--- index_of_an_extra_element.cpp
+++ index_of_an_extra_element.cpp
@@ -9,3 +9,21 @@ int findExtra(int a[], int b[], int n)
     }
     return -1;
 }
+int findExtra(int n, int arr1[], int arr2[])
+{
+    int l = 0;
+    int r = n - 2;
+    while (l <= r)
+    {
+        int mid = l + (r - l) / 2;
+        if (arr1[mid] == arr2[mid])
+        {
+            l = mid + 1;
+        }
+        else
+        {
+            r = mid - 1;
+        }
+    }
+    return l;
+}
