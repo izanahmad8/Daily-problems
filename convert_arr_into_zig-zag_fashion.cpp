a76d242b8f5b04bab205d4e18d4dab944b5a1083
Izan Ahmad <izan.khan596@gmail.com>
2024-06-09 12:02:14 +0530

    dpp

--- convert_arr_into_zig-zag_fashion.cpp
+++ convert_arr_into_zig-zag_fashion.cpp
@@ -12,3 +12,24 @@ void zigZag(int arr[], int n)
         }
     }
 }
+
+void zigZag(int n, vector<int> &arr)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (i % 2 == 0)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                swap(arr[i], arr[i + 1]);
+            }
+        }
+        else
+        {
+            if (arr[i] < arr[i + 1])
+            {
+                swap(arr[i], arr[i + 1]);
+            }
+        }
+    }
+}
