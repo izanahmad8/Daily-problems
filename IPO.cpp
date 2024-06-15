c9aef799ee6951543aa7108aaac04fb341390493
Izan Ahmad <izan.khan596@gmail.com>
2024-06-15 12:14:12 +0530

    leetcode

---
+++ IPO.cpp
@@ -0,0 +1,27 @@
+int findMaximizedCapital(int k, int w, vector<int> &profits, vector<int> &capital)
+{
+    int n = profits.size();
+    vector<pair<int, int>> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        v[i] = {capital[i], profits[i]};
+    }
+    sort(v.begin(), v.end());
+    int i = 0;
+    priority_queue<int> pq;
+    while (k--)
+    {
+        while (i < n && v[i].first <= w)
+        {
+            pq.push(v[i].second);
+            i++;
+        }
+        if (pq.empty())
+        {
+            break;
+        }
+        w += pq.top();
+        pq.pop();
+    }
+    return w;
+}
