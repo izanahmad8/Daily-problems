f583917ba81b6ba660bc02ff43ec8c79b324e5b8
Izan Ahmad <izan.khan596@gmail.com>
2024-06-25 11:30:53 +0530

    dpp

---
+++ left_rotate_matrix_k_times.cpp
@@ -0,0 +1,15 @@
+vector<vector<int>> rotateMatrix(int k, vector<vector<int>> mat)
+{
+    int r = mat.size();
+    int c = mat[0].size();
+    vector<vector<int>> ans(r, vector<int>(c));
+    k = k % c;
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            ans[i][j] = mat[i][(j + k) % c];
+        }
+    }
+    return ans;
+}
