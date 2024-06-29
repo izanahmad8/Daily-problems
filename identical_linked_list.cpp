c421a88d516e783ad6234398b187cfd392cec7a0
Izan Ahmad <izan.khan596@gmail.com>
2024-06-29 11:01:24 +0530

    dpp

--- identical_linked_list.cpp
+++ identical_linked_list.cpp
@@ -18,3 +18,21 @@ bool areIdentical(struct Node *head1, struct Node *head2)
         return false;
     }
 }
+
+bool areIdentical(struct Node *head1, struct Node *head2)
+{
+    Node *p1 = head1;
+    Node *p2 = head2;
+    while (p1 && p2)
+    {
+        if (p1->data != p2->data)
+        {
+            return false;
+        }
+        p1 = p1->next;
+        p2 = p2->next;
+    }
+    if ((!p1 && p2) || (p1 && !p2))
+        return false;
+    return true;
+}
