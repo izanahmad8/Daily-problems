58b5ba5bda31cd9a665c7f475b5363f555c853b2
Izan Ahmad <izan.khan596@gmail.com>
2024-07-01 11:09:49 +0530

    dpp

---
+++ sum_of_left_leaf_nodes.cpp
@@ -0,0 +1,25 @@
+int leftLeavesSum(Node *root)
+{
+    int sum = 0;
+    queue<pair<Node *, bool>> q;
+    q.push({root, false});
+    while (!q.empty())
+    {
+        Node *node = q.front().first;
+        bool chk = q.front().second;
+        q.pop();
+        if (!node->left && !node->right && chk)
+        {
+            sum += node->data;
+        }
+        if (node->left)
+        {
+            q.push({node->left, true});
+        }
+        if (node->right)
+        {
+            q.push({node->right, false});
+        }
+    }
+    return sum;
+}
