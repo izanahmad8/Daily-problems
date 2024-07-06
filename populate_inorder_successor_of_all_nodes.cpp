3b2eed957c57ce57927264612da3fa85c258a1b5
Izan Ahmad <izan.khan596@gmail.com>
2024-07-06 12:27:00 +0530

    dpp

---
+++ populate_inorder_successor_of_all_nodes.cpp
@@ -0,0 +1,13 @@
+Node *prev = NULL;
+void populateNext(Node *root)
+{
+    if (!root)
+        return;
+    populateNext(root->left);
+    if (prev)
+    {
+        prev->next = root;
+    }
+    prev = root;
+    populateNext(root->right);
+}
