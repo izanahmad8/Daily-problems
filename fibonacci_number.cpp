62529f213e06797a699da2ada73199bbf3abc825
Izan Ahmad <izan.khan596@gmail.com>
2024-08-05 21:04:04 +0530

    leetcode

--- fibonacci_number.cpp
+++ fibonacci_number.cpp
@@ -1,12 +1,29 @@
-class Solution
+// top-down approach
+int solve(int n, vector<int> &dp)
 {
-public:
-    int fib(int n)
+    if (n <= 1)
+        return n;
+    if (dp[n] != -1)
+        return dp[n];
+    return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+}
+int fib(int n)
+{
+    vector<int> dp(n + 1, -1);
+    return solve(n, dp);
+}
+
+// bottom-up approach
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    vector<int> dp(n + 1, -1);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++)
     {
-        if (n == 0 || n == 1)
-        {
-            return n;
-        }
-        return {fib(n - 1) + fib(n - 2)};
+        dp[i] = dp[i - 1] + dp[i - 2];
     }
-};
+    return dp[n];
+}
