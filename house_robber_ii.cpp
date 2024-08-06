4a0b3575b29bcf386df065558f5e6bccae68d04d
Izan Ahmad <izan.khan596@gmail.com>
2024-08-06 20:04:38 +0530

    leetcode

--- house_robber_ii.cpp
+++ house_robber_ii.cpp
@@ -1,3 +1,4 @@
+// top down appraoch
 int solve(int i, vector<int> &nums, vector<int> &dp, int n)
 {
     if (i >= n)
@@ -20,4 +21,29 @@ int rob(vector<int> &nums)
     fill(dp.begin(), dp.end(), -1);
     int noTake_0th = solve(1, nums, dp, n);
     return max(take_0th, noTake_0th);
+}
+
+// bottom up approach
+int rob(vector<int> &nums)
+{
+    int n = nums.size();
+    if (n == 1)
+        return nums[0];
+    if (n == 2)
+        return max(nums[0], nums[1]);
+    vector<int> dp(n + 1, 0);
+    dp[1] = nums[0];
+    for (int i = 2; i < n; i++)
+    {
+        dp[i] = max(nums[i - 1] + dp[i - 2], dp[i - 1]);
+    }
+    int take = dp[n - 1];
+    fill(dp.begin(), dp.end(), 0);
+    dp[2] = nums[1];
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = max(nums[i - 1] + dp[i - 2], dp[i - 1]);
+    }
+    int no_take = dp[n];
+    return max(take, no_take);
 }
