c0fffa8764ca5130a23500f1a1b771bec85be678
Izan Ahmad <izan.khan596@gmail.com>
2024-08-22 15:09:20 +0530

    leetcode

--- maximum_length_of_pair_chain.cpp
+++ maximum_length_of_pair_chain.cpp
@@ -24,3 +24,24 @@ int findLongestChain(vector<vector<int>> &pairs)
     sort(pairs.begin(), pairs.end());
     return solve(pairs, 0, -1, dp);
 }
+
+// bottom-up approach
+int findLongestChain(vector<vector<int>> &pairs)
+{
+    int n = pairs.size();
+    sort(pairs.begin(), pairs.end());
+    vector<int> dp(n, 1);
+    int maxLis = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (pairs[i][0] > pairs[j][1])
+            {
+                dp[i] = max(dp[i], dp[j] + 1);
+                maxLis = max(maxLis, dp[i]);
+            }
+        }
+    }
+    return maxLis;
+}
