5a853231e04ccb77664c936c0cc9ba203695a553
Izan Ahmad <izan.khan596@gmail.com>
2024-08-28 22:32:05 +0530

    dpp

---
+++ sort_element_of_an_array_by_frequency.cpp
@@ -0,0 +1,27 @@
+vector<int> sortByFreq(vector<int> &arr)
+{
+    map<int, int> m;
+    for (auto i : arr)
+    {
+        m[i]++;
+    }
+    vector<pair<int, int>> v;
+    for (auto it : m)
+    {
+        v.push_back({it.second, it.first});
+    }
+    sort(v.begin(), v.end(), [&](pair<int, int> &a, pair<int, int> &b)
+         {
+            if(a.first == b.first){return a.second < b.second;}
+            return a.first > b.first; });
+    vector<int> ans;
+    for (auto x : v)
+    {
+        int f = x.first, el = x.second;
+        for (int i = 0; i < f; i++)
+        {
+            ans.push_back(el);
+        }
+    }
+    return ans;
+}
