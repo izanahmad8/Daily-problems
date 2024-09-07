4d4515002350656b4187cc4cd32d12f63ef9ea8c
Izan Ahmad <izan.khan596@gmail.com>
2024-09-07 21:55:56 +0530

    leetcode

--- edit_distance.cpp
+++ edit_distance.cpp
@@ -1,31 +1,30 @@
-int solve(string &s1, string &s2, int i, int j, vector<vector<int>> &dp)
+int solve(int i, int j, string &s1, string &s2, vector<vector<int>> &dp)
 {
-    if (i == s1.size())
+    if (i >= s1.size())
+    {
         return s2.size() - j;
-    if (j == s2.size())
+    }
+    if (j >= s2.size())
+    {
         return s1.size() - i;
+    }
     if (dp[i][j] != -1)
     {
         return dp[i][j];
     }
-    int ans = INT_MAX;
-    if (s1[i] != s2[j])
+    if (s1[i] == s2[j])
     {
-        int a = 1 + solve(s1, s2, i + 1, j, dp);     // deletion
-        int b = 1 + solve(s1, s2, i, j + 1, dp);     // insertion
-        int c = 1 + solve(s1, s2, i + 1, j + 1, dp); // substitution
-        ans = min({ans, a, b, c});
+        return dp[i][j] = solve(i + 1, j + 1, s1, s2, dp);
     }
-    else
-    {
-        return dp[i][j] = solve(s1, s2, i + 1, j + 1, dp);
-    }
-    return dp[i][j] = ans;
+    int t1 = 1 + solve(i, j + 1, s1, s2, dp);
+    int t2 = 1 + solve(i + 1, j, s1, s2, dp);
+    int t3 = 1 + solve(i + 1, j + 1, s1, s2, dp);
+    return dp[i][j] = min({t1, t2, t3});
 }
-int editDistance(string str1, string str2)
+int minDistance(string word1, string word2)
 {
-    int n = str1.size();
-    int m = str2.size();
+    int n = word1.size();
+    int m = word2.size();
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-    return solve(str1, str2, 0, 0, dp);
+    return solve(0, 0, word1, word2, dp);
 }
