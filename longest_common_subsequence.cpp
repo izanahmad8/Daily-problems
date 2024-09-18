346faed6b5213d905f26ada745c38de3a3480ff2
Izan Ahmad <izan.khan596@gmail.com>
2024-09-18 15:18:10 +0530

    leetcode

--- longest_common_subsequence.cpp
+++ longest_common_subsequence.cpp
@@ -25,3 +25,26 @@ int lcs(int n, int m, string str1, string str2)
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
     return solve(0, 0, n, m, str1, str2, dp);
 }
+
+// bottom up approach - leetcode
+int longestCommonSubsequence(string text1, string text2)
+{
+    int n = text1.size();
+    int m = text2.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (text1[i - 1] == text2[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[n][m];
+}
