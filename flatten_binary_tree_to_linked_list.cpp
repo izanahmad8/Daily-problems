4f92be89dcaa367cf73058c1e09b30ba2c4842ae
Izan Ahmad <izan.khan596@gmail.com>
2024-09-28 15:06:46 +0530

    leetcode

---
+++ flatten_binary_tree_to_linked_list.cpp
@@ -0,0 +1,11 @@
+TreeNode *prev = NULL;
+void flatten(TreeNode *root)
+{
+    if (!root)
+        return;
+    flatten(root->right);
+    flatten(root->left);
+    root->right = prev;
+    root->left = NULL;
+    prev = root;
+}
