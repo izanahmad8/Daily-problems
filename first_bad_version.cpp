8e6e23e908aedf33be59cabd0f21a1b066244898
Izan Ahmad <izan.khan596@gmail.com>
2024-09-29 15:37:25 +0530

    leetcode

---
+++ first_bad_version.cpp
@@ -0,0 +1,18 @@
+int firstBadVersion(int n)
+{
+    int l = 1;
+    int r = n;
+    while (l < r)
+    {
+        int m = l + (r - l) / 2;
+        if (isBadVersion(m))
+        {
+            r = m;
+        }
+        else
+        {
+            l = m + 1;
+        }
+    }
+    return l;
+}
