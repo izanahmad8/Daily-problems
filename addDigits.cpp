8f2efc327744369ac285eba30c59d0fdb4027de5
Izan Ahmad <izan.khan596@gmail.com>
2024-09-29 15:23:49 +0530

    leetcode

---
+++ addDigits.cpp
@@ -0,0 +1,15 @@
+int addDigits(int num)
+{
+    int sum = 0;
+    if (num < 10)
+    {
+        return num;
+    }
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    int ans = addDigits(sum);
+    return ans;
+}
