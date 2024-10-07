27ea5ead4a9b49257c52d37d471513ae722ee4db
Izan Ahmad <izan.khan596@gmail.com>
2024-10-07 12:53:14 +0530

    dpp

---
+++ xor_linked_list.cpp
@@ -0,0 +1,18 @@
+struct Node *insert(struct Node *head, int data)
+{
+    Node *newHead = new Node(data);
+    newHead->npx = head;
+    return newHead;
+}
+
+vector<int> getList(struct Node *head)
+{
+    vector<int> ans;
+    Node *curr = head;
+    while (curr)
+    {
+        ans.push_back(curr->data);
+        curr = curr->npx;
+    }
+    return ans;
+}
