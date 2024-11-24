1ffee78811831bb894ef0f4d3758afaaa6c691d8
Izan Ahmad <izan.khan596@gmail.com>
2024-11-24 12:47:13 +0530

    dpp

--- Kadane's_Algorithm.cpp
+++ Kadane's_Algorithm.cpp
@@ -1,11 +1,11 @@
-long long maxSubarraySum(vector<int>arr){
-        
-        int res=arr[0];
-        int max_ending=arr[0];
-        for(int i=1;i<arr.size();i++)
-        {
-            max_ending=max(max_ending+arr[i],arr[i]);
-            res=max(max_ending,res);
-        }
-        return res;
+int maxSubarraySum(vector<int> &arr)
+{
+    int maxSum = arr[0];
+    int currSum = arr[0];
+    for (int i = 1; i < arr.size(); i++)
+    {
+        currSum = max(currSum + arr[i], arr[i]);
+        maxSum = max(currSum, maxSum);
     }
+    return maxSum;
+}
