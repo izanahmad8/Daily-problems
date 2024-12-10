3fc52f0ef126e2af612ec45a03b7671f05b1381b
Izan Ahmad <izan.khan596@gmail.com>
2024-12-10 23:24:56 +0530

    dpp

---
+++ non-overlapping_intervals.cpp
@@ -0,0 +1,19 @@
+int minRemoval(vector<vector<int>> &intervals)
+{
+    sort(intervals.begin(), intervals.end());
+    int cnt = 0;
+    int end = intervals[0][1];
+    for (int i = 1; i < intervals.size(); i++)
+    {
+        if (intervals[i][0] < end)
+        {
+            cnt++;
+            end = min(intervals[i][1], end);
+        }
+        else
+        {
+            end = intervals[i][1];
+        }
+    }
+    return cnt;
+}
