17b5748ba74ed90c3b6c2f107bef3d41fa5f06a5
Izan Ahmad <izan.khan596@gmail.com>
2024-12-15 20:05:59 +0530

    dpp

--- peak_element.cpp
+++ peak_element.cpp
@@ -1,27 +1,6 @@
-int peakElement(int arr[], int n)
+int peakElement(vector<int> &arr)
 {
-    int low = 0, high = n - 1;
-    while (low <= high)
-    {
-        int mid = (low + high) / 2;
-        if ((mid == 0 || arr[mid - 1] <= arr[mid]) && (mid == n - 1 || arr[mid + 1] <= arr[mid]))
-        {
-            return mid;
-        }
-        else if (mid > 0 && arr[mid - 1] >= arr[mid])
-        {
-            high = mid - 1;
-        }
-        else
-        {
-            low = mid + 1;
-        }
-    }
-    return -1;
-}
-
-int peakElement(int arr[], int n)
-{
+    int n = arr.size();
     if (n == 1)
         return 0;
     int low = 0, high = n - 1;
