b8bfcbe79647ac62c776f683ac23fa7a9bf5fb7b
Izan Ahmad <izan.khan596@gmail.com>
2025-05-28 23:10:23 +0530

    dpp

---
+++ find_rectangle_as_corner_as_1.cpp
@@ -0,0 +1,20 @@
+bool ValidCorner(vector<vector<int>> &mat)
+{
+    int n = mat.size();
+    int m = mat[0].size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int cnt = 0;
+            for (int k = 0; k < m; k++)
+            {
+                if (mat[i][k] + mat[j][k] == 2)
+                    cnt++;
+            }
+            if (cnt >= 2)
+                return true;
+        }
+    }
+    return false;
+}
