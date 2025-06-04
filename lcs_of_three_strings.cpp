63ff71689ae8b911069db42ef755bc48c7fbb887
Izan Ahmad <izan.khan596@gmail.com>
2025-06-04 14:09:58 +0530

    dpp

--- lcs_of_three_strings.cpp
+++ lcs_of_three_strings.cpp
@@ -1,9 +1,4 @@
-int LCSof3(string A, string B, string C, int n1, int n2, int n3)
-{
-    vector<vector<vector<int>>> dp(n1, vector<vector<int>>(n2, vector<int>(n3, -1)));
-    return solve(A, B, C, 0, 0, 0, n1, n2, n3, dp);
-}
-int solve(string A, string B, string C, int i, int j, int k, int n1, int n2, int n3, vector<vector<vector<int>>> &dp)
+int solve(string &A, string &B, string &C, int i, int j, int k, int n1, int n2, int n3, vector<vector<vector<int>>> &dp)
 {
     if (i == n1 || j == n2 || k == n3)
     {
@@ -21,4 +16,12 @@ int solve(string A, string B, string C, int i, int j, int k, int n1, int n2, int
     int b = solve(A, B, C, i, j + 1, k, n1, n2, n3, dp);
     int c = solve(A, B, C, i, j, k + 1, n1, n2, n3, dp);
     return dp[i][j][k] = max({a, b, c});
+}
+int lcsOf3(string &s1, string &s2, string &s3)
+{
+    int n1 = s1.size();
+    int n2 = s2.size();
+    int n3 = s3.size();
+    vector<vector<vector<int>>> dp(n1, vector<vector<int>>(n2, vector<int>(n3, -1)));
+    return solve(s1, s2, s3, 0, 0, 0, n1, n2, n3, dp);
 }
