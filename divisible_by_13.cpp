d1bc93280464791ebff9bb411aff97d3aa44d3bc
Izan Ahmad <izan.khan596@gmail.com>
2025-07-15 14:48:02 +0530

    dpp

---
+++ divisible_by_13.cpp
@@ -0,0 +1,10 @@
+bool divby13(string &s)
+{
+    int rem = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        int dig = s[i] - '0';
+        rem = (rem * 10 + dig) % 13;
+    }
+    return rem == 0;
+}
