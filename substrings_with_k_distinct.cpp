b9045b329a02127a657d0c9c2a4d4db73d0cdb16
Izan Ahmad <izan.khan596@gmail.com>
2026-01-10 22:51:56 +0530

    dpp

--- substrings_with_k_distinct.cpp
+++ substrings_with_k_distinct.cpp
@@ -1,24 +1,30 @@
+int atMost(string &s, int k)
+{
+    vector<int> freq(26, 0);
+    int cnt = 0;
+    int res = 0;
+    int i = 0;
+    for (int j = 0; j < s.size(); j++)
+    {
+        freq[s[j] - 'a']++;
+        if (freq[s[j] - 'a'] == 1)
+        {
+            cnt++;
+        }
+        while (cnt > k)
+        {
+            freq[s[i] - 'a']--;
+            if (freq[s[i] - 'a'] == 0)
+            {
+                cnt--;
+            }
+            i++;
+        }
+        res += j - i + 1;
+    }
+    return res;
+}
 int countSubstr(string &s, int k)
 {
-    int count = 0;
-    for (int i = 0; i < s.size(); i++)
-    {
-        vector<int> freq(26, 0);
-        int distinct = 0;
-        for (int j = i; j < s.size(); j++)
-        {
-            if (freq[s[j] - 'a'] == 0)
-            {
-                distinct++;
-            }
-            freq[s[j] - 'a']++;
-            if (distinct == k)
-            {
-                count++;
-            }
-            if (distinct > k)
-                break;
-        }
-    }
-    return count;
+    return atMost(s, k) - atMost(s, k - 1);
 }
