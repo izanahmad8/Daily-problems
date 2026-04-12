cba62f0c4e84caefd59a9391456c305676859833
Izan Ahmad <izan.khan596@gmail.com>
2026-04-12 22:02:00 +0530

    dpp

--- toeplitz_matrix.cpp
+++ toeplitz_matrix.cpp
@@ -2,7 +2,7 @@ bool isToeplitz(vector<vector<int>> &mat)
 {
     for (int i = 1; i < mat.size(); i++)
     {
-        for (int j = 1; j < mat[i].size(); j++)
+        for (int j = 1; j < mat[0].size(); j++)
         {
             if (mat[i][j] != mat[i - 1][j - 1])
             {
